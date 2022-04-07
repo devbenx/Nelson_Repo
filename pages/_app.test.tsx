28220d658c76e60f5003610692319ff9aff59906
Frank Benjamin <benjaminarias3197@gmail.com>
2022-04-07 13:39:58 +0200

    deleted  unused folder

--- pages/_app.test.tsx
+++ pages/_app.test.tsx
@@ -1,20 +1,20 @@
-// import React from "react";
-// import { render } from "@testing-library/react";
-// import DisplayModal from "./components/displaymodal";
+import React from "react";
+import { render } from "@testing-library/react";
+import DisplayModal from "./components/displaymodal";
 
-// const mockedPorduct = {
-//         id: 12345,
-//         img: 'no_url',
-//         brand: 'brand',
-//         color: 'somecolor',
-//         size: '40',
-//         category: 'leather',
-//         price: 40,
-// }
+const mockedPorduct = {
+        id: 12345,
+        img: 'no_url',
+        brand: 'brand',
+        color: 'somecolor',
+        size: '40',
+        category: 'leather',
+        price: 40,
+}
 
-// test("Renders modal", async () => {
-//         const { findByText } = render(<DisplayModal openModal={true} product={mockedPorduct} />);
-//         const element = await findByText(/TOEGEVOEGD AAN JE WINKELMANDJE!/);
-//         expect(element).toBeInTheDocument();
-// });
-export { }
+test("Renders modal", async () => {
+        const { findByText } = render(<DisplayModal openModal={true} product={mockedPorduct} />);
+        const element = await findByText(/TOEGEVOEGD AAN JE WINKELMANDJE!/);
+        expect(element).toBeInTheDocument();
+});
+// export { }
